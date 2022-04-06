8f4106f11dc23c212fd8ca6be2373ca2a0a7c49f
Heber Chavez <sazas9696@gmail.com>
2022-04-07 01:12:41 +0200

    Finished homework JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -104,7 +104,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-
+  if (Math.floor(numero)===numero) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -154,7 +158,20 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+  if(numero < 2){
+    return false;
+  }
+  if(numero===2){
+    return true;
+  }
+    for(var i = 2; i < numero;i++){
+      if (numero%i===0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -179,13 +196,25 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero > 99 && numero < 1000) {
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  i = 0;
+do{
+  numero+=5;
+  i++
+}
+while(i<8);
+return numero;
 }
 
 
